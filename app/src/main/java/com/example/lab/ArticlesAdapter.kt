1df0e5cd99bf3948c1d85406afcaa1e5831c0b29
rockzoot <ahmed.ai2002@subrays.com>
2024-10-13 01:25:45 +0500

    DetailsActivity

--- app/src/main/java/com/example/lab/ArticlesAdapter.kt
+++ app/src/main/java/com/example/lab/ArticlesAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.lab
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,7 +24,17 @@ class ArticlesAdapter(val articles: List<Article>): RecyclerView.Adapter<Article
         holder.propertyPrice.text = article.price
         holder.propertyDescription.text = article.description
         holder.propertyLocation.text = article.location
+        holder.itemView.setOnClickListener {
+            Log.d("Testing", "clicked")
+            val intent = Intent(holder.itemView.context, ArticleDetailsActivity::class.java)
+            intent.putExtra("title", article.id)
+            intent.putExtra("category", article.name)
+            intent.putExtra("date", article.price)
+            intent.putExtra("image", article.image)
+            intent.putExtra("detail", article.description)
 
+            holder.itemView.context.startActivity(intent)
+        }
 
 
 
